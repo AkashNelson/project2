30e2aed1891bc39cfb95c08d40ceade7448584e0
AkashNelson <akashnelson991@gmail.com>
2021-11-10 21:55:10 +0530

    sdg

---
+++ example3/org/phone/empoloyeeInfo.java
@@ -0,0 +1,15 @@
+package org.phone;
+
+public class empoloyeeInfo {
+	public void companyName(long pn,String name,double d) {
+		System.out.println("your employeeId "+pn);
+		System.out.println("your name "+name);
+		System.out.println("your salary "+d);
+
+	}
+public static void main(String[] args) {
+	empoloyeeInfo g=new empoloyeeInfo();
+	g.companyName(902500084, "akashnelson", 23.45);
+	
+}
+}
---
+++ example5/org/add/greenTech.java
@@ -0,0 +1,25 @@
+package org.add;
+
+public class greenTech 
+{
+ private void greensOmr(int a) 
+ {
+	System.out.println("java training");
+}
+ private void greensOmr(String b) 
+ {
+	System.out.println("aws  training");
+}
+ 
+private void greensOmr(long c) 
+ {
+	System.out.println("python training");
+}
+ public static void main(String[] args) {
+	greenTech j=new greenTech();
+	j.greensOmr(1);
+	j.greensOmr("a");
+	j.greensOmr(733338);
+}
+ 
+}
